768737a9938e9a0cce1d17b636b3763a061634fc
L226n <lu.00e2n@gmail.com>
2026-05-21 23:47:26 +0100

    yesss

--- home/homemain.js
+++ home/homemain.js
@@ -14,8 +14,8 @@ var crosshair = document.getElementById("crosshair");
 var gunshot = new Audio("kirk/died.mp3");
 var bg = new Audio("music.mp3");
 bg.loop = true;
-bg.play()
 document.body.onpointermove = event => {
+	bg.play()
 	const { clientX, clientY } = event;
 		crosshair.animate({
 		    left: `${clientX-(crosshair.width/2)}px`,
