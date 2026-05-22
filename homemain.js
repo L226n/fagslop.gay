2ade575fc26756dee4f12a14bba008e5c4caeaf5
L226n <lu.00e2n@gmail.com>
2026-05-22 12:21:15 +0100

    fdsj

--- homemain.js
+++ homemain.js
@@ -9,16 +9,22 @@ function saveKirk() {
 function noAudio() {
 	bg.pause();
 }
+function siteBegin() {
+	document.getElementById("entered").style="display:block;";
+	document.getElementById("image").style="display:none;";
+	document.querySelector("html").style="background-image: url('background.png')";
+	bg.play()
+}
 var kirk = document.getElementById("kirky");
 var crosshair = document.getElementById("crosshair");
 var gunshot = new Audio("kirk/died.mp3");
 var bg = new Audio("music.mp3");
 bg.loop = true;
 document.body.onpointermove = event => {
-	bg.play()
 	const { clientX, clientY } = event;
 		crosshair.animate({
 		    left: `${clientX-(crosshair.width/2)}px`,
 		    top: `${clientY-(crosshair.height/2)}px`
 	    }, {duration: 0, fill: "forwards"})
 }
+Marquee3k.init()
